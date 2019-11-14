25127d7dc0203f9d74769d7929d3f903e13bd7db
TJ Holt <troy.j.holt@gmail.com>
2019-11-14 13:01:01 -0800

    made changes

--- Week7/assignment5/assignment5.js
+++ Week7/assignment5/assignment5.js
@@ -15,45 +15,14 @@ app.get('/',function(req,res){
   res.render('home');
 });
 
-app.get('/show-data',function(req,res){
+app.get('/gData',function(req,res){
   var context = {};
-  context.sentData = req.query.myData;
-  res.render('show-data', context);
+  context.someData = req.query.myData;
+  //res.render('getData', context);
+    res.status(gData).send('Get Data Recieved');
+    res.render('home');
 });
 
-app.get('/get-loopback',function(req,res){
-  var qParams = "";
-  for (var p in req.query){
-    qParams += "The name " + p + " contains the value " + req.query[p] + ", ";
-  }
-  qParams = qParams.substring(0,qParams.lastIndexOf(','));
-  qParams += '.';
-  var context = {};
-  context.dataList = qParams;
-  res.render('get-loopback', context);
-});
-
-app.get('/get-loopback-improved',function(req,res){
-  var qParams = [];
-  for (var p in req.query){
-    qParams.push({'name':p,'value':req.query[p]})
-  }
-  var context = {};
-  context.dataList = qParams;
-  res.render('get-loopback-improved', context);
-});
-
-app.post('/post-loopback', function(req,res){
-  var qParams = [];
-  for (var p in req.body){
-    qParams.push({'name':p,'value':req.body[p]})
-  }
-  console.log(qParams);
-  console.log(req.body);
-  var context = {};
-  context.dataList = qParams;
-  res.render('post-loopback', context);
-});
 
 app.use(function(req,res){
   res.status(404);
