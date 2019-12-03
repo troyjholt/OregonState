8791888cd8d235ed5c19bddb3f002e16e40488d3
TJ Holt <troy.j.holt@gmail.com>
2019-12-03 15:47:54 -0800

    cadf

--- Week9/assignment6/assignment6.js
+++ Week9/assignment6/assignment6.js
@@ -24,32 +24,6 @@ app.get('/',function(req,res){
   res.render('home');
 });
 
-
-app.get('/gData', function(req, res) {
-    var context = {};
-    context.dataType = "GET";
-    context.param = [];
-    for (var p in req.query)
-    {
-        context.param.push({'name':p,'value':req.query[p]})
-    }
-    context.dataList = context.param;
-    res.render('gData', context);
-});
-
-app.post('/gData', function (req, res)
-{
-    var context = {};
-    context.dataType = "POST";
-    context.pParam = [];
-    for (var p in req.body)
-    {
-        context.pParam.push({'name':p,'value':req.body[p]})
-    }
-    context.dataList = context.pParam;
-    res.render('gData', context)
-});
-
 app.use(function(req,res){
   res.status(404);
   res.render('404');
