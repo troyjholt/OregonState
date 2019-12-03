ebdee8eb8fa8dfa6693a41c1388fc847434264df
TJ Holt <troy.j.holt@gmail.com>
2019-12-03 15:02:17 -0800

    Update assignment6.js

--- Week9/assignment6/assignment6.js
+++ Week9/assignment6/assignment6.js
@@ -88,7 +88,7 @@ app.get('/', function (req, res, next)
     });
 });
 
-mysql.pool.query(
+pool.query(
     'CREATE TABLE IF NOT EXISTS workouts(' +
     'id INT PRIMARY KEY AUTO_INCREMENT,' +
     'name VARCHAR(255) NOT NULL,' +
