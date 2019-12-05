462459bc2eb2cda495c58a479ce19aef081a7260
TJ Holt <troy.j.holt@gmail.com>
2019-12-05 09:39:47 -0800

    fdsadf

--- Week9/assignment6/assignment6.js
+++ Week9/assignment6/assignment6.js
@@ -7,16 +7,16 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-/*var mysql = require('mysql');
+var mysql = require('mysql');
 var pool = mysql.createPool({
     connectionLimit: 10,
     host: 'classmysql.engr.oregonstate.edu',
     user: 'cs290_holttr',
     password: '6329',
     database: 'cs290_holttr'
-});*/
+});
 
-//module.exports.pool = pool;
+module.exports.pool = pool;
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -32,6 +32,36 @@ app.post('/insert', function (req, res, next)
     var context = {};
 
     console.log('executing post');
+        pool.query(
+        'create table if not exists workouts(' +
+        'id int primary key auto_increment,' +
+        'name varchar(255) not null,' +
+        'reps int,' +
+        'weight int,' +
+        'date date,' +
+        'lbs boolean)',
+        function (err, result)
+        {
+            if (err) throw err;
+            console.log('table created');
+        }
+    );
+    pool.query("insert into workouts (`name`, `reps`, `weight`, `date`, `lbs`) values (?, ?, ?, ?, ?)",
+        [req.body.name, req.body.reps, req.body.weight, req.body.date, req.body.unit],
+
+        function (err, result)
+        {
+            if (err)
+            {
+                next(err);
+                return;
+            }
+            console.log("post worked maybe");
+            context.results = "inserted id " + result.insertid;
+            context.inserted = result.insertid
+            res.send(json.stringify(context));
+            res.render('workout', context);
+        });
 
     res.send("hello");
 });
