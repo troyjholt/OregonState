db53fd88fd25b3628ff4b3efe72016cd422c5e22
TJ Holt <troy.j.holt@gmail.com>
2019-12-04 16:01:02 -0800

    gfdsgdf

--- Week9/assignment6/assignment6.js
+++ Week9/assignment6/assignment6.js
@@ -29,7 +29,7 @@ app.get('/', function (req, res)
 app.post('/insert', function (req, res)
 {
     var context = {};
-    context.dataType = "POST";
+    context.dataType = "It is Posting";
     
     console.log(context.dataType);
     res.render('home', context)
