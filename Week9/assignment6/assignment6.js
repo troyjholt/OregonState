e930138e5b43eed952dd918ee01184e9e1e6b28a
TJ Holt <troy.j.holt@gmail.com>
2019-12-05 18:39:48 -0800

    Update assignment6.js

--- Week9/assignment6/assignment6.js
+++ Week9/assignment6/assignment6.js
@@ -78,10 +78,11 @@ app.get('/insert', function (req, res, next)
             return;
         }
         context = JSON.stringify(rows);
-        console.log(rows[0].RowDataPacket);
+        console.log(rows[0]['RowDataPacket']);
         res.send(context);
         res.render('workout');
     });
+    console.log("check below Losers!");
     console.log('executing GET');
 });
 
